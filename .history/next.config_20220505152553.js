34571b818174764bff5c993353cfa638029c7651
Dewey Bennett <debennett3@gmail.com>
2022-05-05 16:06:11 -0400

    v3.2

---
+++ .history/next.config_20220505152553.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos'],
+  },
+}
+
+module.exports = nextConfig
