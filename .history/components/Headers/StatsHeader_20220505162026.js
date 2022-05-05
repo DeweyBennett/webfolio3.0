a8d124a2b978b969790183bff12a07349f3faafc
Dewey Bennett <debennett3@gmail.com>
2022-05-05 16:27:10 -0400

    3.3

---
+++ .history/components/Headers/StatsHeader_20220505162026.js
@@ -0,0 +1,81 @@
+import React from "react";
+import CardStats from "../Cards/StatsCard";
+
+
+export default function HeaderStats({ blogs, portfolios, leads }) {
+
+  let BlogTotalVisits = 0;
+  let PortfolioTotalVisits = 0;
+  let totalLeads = leads.length;
+  
+  blogs.forEach(function(item){
+    BlogTotalVisits += item.visits;
+  })
+
+  portfolios.forEach(function(item){
+    PortfolioTotalVisits += item.visits;
+  })
+
+  return (
+    <>
+      {/* Header */}
+      <div className="relative bg-gray-800 md:pt-32 pb-32 pt-12">
+        <div className="px-4 md:px-10 mx-auto w-full">
+          <div>
+            {/* Card stats */}
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+                <CardStats
+                  statSubtitle="Blog Views"
+                  statTitle= {BlogTotalVisits}
+                  statArrow="up"
+                  statPercent="3.48"
+                  statPercentColor="text-emerald-500"
+                  statDescripiron="Since last month"
+                  statIconName="far fa-chart-bar"
+                  statIconColor="bg-red-500"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+                <CardStats
+                  statSubtitle="Portfolio Views"
+                  statTitle={PortfolioTotalVisits}
+                  statArrow="down"
+                  statPercent="3.48"
+                  statPercentColor="text-red-500"
+                  statDescripiron="Since last week"
+                  statIconName="fas fa-chart-pie"
+                  statIconColor="bg-orange-500"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+                <CardStats
+                  statSubtitle="Pipeline Total"
+                  statTitle={totalLeads}
+                  statArrow="down"
+                  statPercent="1.10"
+                  statPercentColor="text-orange-500"
+                  statDescripiron="Since yesterday"
+                  statIconName="fas fa-users"
+                  statIconColor="bg-pink-500"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+                <CardStats
+                  statSubtitle="Deals"
+                  statTitle="COMING SOON"
+                  statArrow="up"
+                  statPercent="12"
+                  statPercentColor="text-emerald-500"
+                  statDescripiron="Since last month"
+                  statIconName="fas fa-percent"
+                  statIconColor="bg-blue-500"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
