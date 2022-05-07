a11e183a5a379f2dd77f992d7a3ba56190161c3c
DeweyBennett <100319680+DeweyBennett@users.noreply.github.com>
2022-05-07 12:08:37 -0400

    Delete .history directory

--- .history/next.config_20220505152553.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos'],
-  },
-}
-
-module.exports = nextConfig
